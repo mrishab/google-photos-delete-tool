ad67d9afa41213f4d024f79d2c36a32eb034c2f0
Rishab Manocha <rishab.manocha@outlook.com>
2019-01-09 13:47:10 -0800

    Structured the code to be more organized. Put the configs and buttons in objects. Changed the selector for the delete button to use a shorter and cleaner value. Moved the second setTimeout function call for confirmation inside the deleteButton call

--- delete_photos.js
+++ delete_photos.js
@@ -1,19 +1,27 @@
 // Selector for Images and buttons
-const DATE_CHECKBOXES_CLASS = '.ckGgle';
-const DELETE_BUTTON_SELECTOR = '#yDmH0d > c-wiz > c-wiz.QtDoYb.KWdEHf.u5a4d.g7of6e.maPcY > content > div.c9yG5b.txMZRd > div > div:nth-child(3) > button';
-const CONFIRMATION_BOX_SELECTOR = '#yDmH0d > div.llhEMd.iWO5td > div > div.g3VIld.V639qd.bvQPzd.oEOLpc.Up8vH.J9Nfi.A9Uzve.iWO5td > div.XfpsVe.J9fJmf > button.VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.nCP5yc.DuMIQc.kHssdc.HvOprf';
+const ELEMENT_SELECTORS = {
+    checkboxClass: '.ckGgle',
+    deleteButton: 'button[title="Delete"]',
+    confirmationButton: '#yDmH0d > div.llhEMd.iWO5td > div > div.g3VIld.V639qd.bvQPzd.oEOLpc.Up8vH.J9Nfi.A9Uzve.iWO5td > div.XfpsVe.J9fJmf > button.VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.nCP5yc.DuMIQc.kHssdc.HvOprf'
+}
 
 // Time Configuration (in milliseconds)
-const DELETE_CYCLE_DELAY = 7000;
-const PRESS_DELETE_DELAY = 1000;
-const PRESS_CONFIRMATION_DELAY = 2000;
+const TIME_CONFIG = {
+    delete_cycle: 7000,
+    press_button_delay: 1000
+};
 
-let checkboxes, deleteButton, confirmation_button;
-let numOfImages = 0;
+let imageCount = 0;
+
+let checkboxes;
+let buttons = {
+    deleteButton: null,
+    confirmationButton: null
+}
 
 let deleteTask = setInterval(() => {
 
-    checkboxes = document.querySelectorAll(DATE_CHECKBOXES_CLASS);
+    checkboxes = document.querySelectorAll(ELEMENT_SELECTORS['checkboxClass']);
 
     if (checkboxes.length <= 0) {
         console.log("[INFO] No more images to delete.");
@@ -22,19 +30,19 @@ let deleteTask = setInterval(() => {
         return;
     }
 
-    numOfImages += checkboxes.length;
+    imageCount += checkboxes.length;
 
-    checkboxes.forEach((checkbox) => {checkbox.click()});
+    checkboxes.forEach((checkbox) => { checkbox.click() });
     console.log("[INFO] Deleting", checkboxes.length, "images");
 
-    setTimeout(()=>{
-        deleteButton = document.querySelector(DELETE_BUTTON_SELECTOR);
-        deleteButton.click();
-    }, PRESS_DELETE_DELAY);
-
     setTimeout(() => {
-        confirmation_button = document.querySelector(CONFIRMATION_BOX_SELECTOR);
-        confirmation_button.click();
-    }, PRESS_CONFIRMATION_DELAY);
-},
-DELETE_CYCLE_DELAY);
+
+        buttons.deleteButton = document.querySelector(ELEMENT_SELECTORS['deleteButton']);
+        buttons.deleteButton.click();
+
+        setTimeout(() => {
+            buttons.confirmation_button = document.querySelector(ELEMENT_SELECTORS['confirmationButton']);
+            buttons.confirmation_button.click();
+        }, TIME_CONFIG['press_button_delay']);
+    }, TIME_CONFIG['press_button_delay']);
+}, TIME_CONFIG['delete_cycle']);
